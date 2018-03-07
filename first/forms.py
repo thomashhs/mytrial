10abd4baa50bd5d4c2c16a3967a0da75739df755
thomashhs <345870016@qq.com>
2018-03-08 00:29:36 +0800

    third commit

--- first/forms.py
+++ first/forms.py
@@ -4,3 +4,8 @@ class CompundForm(forms.Form):
     base_money=forms.IntegerField(label='本金')
     base_rate=forms.DecimalField(label='年利率')
     base_year=forms.IntegerField(label='年期')
+
+class LoanForm(forms.Form):
+    loan_money=forms.IntegerField(label='贷款总额')
+    loan_year=forms.IntegerField(label='贷款年限')
+    loan_rate=forms.DecimalField(label='商贷利率')
--- first/views.py
+++ first/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Title
 from django.http import HttpResponse
 import math
-from .forms import CompundForm
+from .forms import CompundForm,LoanForm
 
 # Create your views here.
 def index(request):
@@ -15,14 +15,16 @@ def detail(request,title_id):
     if title.title_text=='复利计算器':
         form=CompundForm()
         return render(request, 'first/detail_1.html', context={'title': title,'form':form})
-
-
+    if title.title_text=='住房贷款计算':
+        form=LoanForm()
+        return render(request, 'first/detail_1.html', context={'title': title, 'form': form})
     return render(request,'first/detail.html',context={'title':title})
 
 
 
 def result(request,title_id):
     title = get_object_or_404(Title, pk=title_id)
+
     if title.title_text == '复利计算器':
         if request.method=='POST':
             form=CompundForm(request.POST)
@@ -36,11 +38,26 @@ def result(request,title_id):
                 result_interest = result_money-base_money
 
                 return render(request, 'first/result_1.html', context={'title':title,
-                                                               'base_money': base_money,
-                                                               'base_rate':base_rate,
-                                                               'base_year':base_year,
+                                                               'form':form,
                                                                'result_money':result_money,
                                                                'result_interest':result_interest})
+
+    if title.title_text == '住房贷款计算':
+        if request.method == 'POST':
+            form = LoanForm(request.POST)
+
+            if form.is_valid():
+                loan_money = form.cleaned_data['loan_money']
+                loan_rate = form.cleaned_data['loan_rate']/12
+                loan_year = form.cleaned_data['loan_year']
+
+                result_month = loan_year*12
+                result_avg_money = (loan_money*loan_rate*pow(1+loan_rate,result_month))/ (pow(1+loan_rate,result_month)-1)
+
+                return render(request, 'first/result_2.html', context={'title': title,
+                                                                       'form': form,
+                                                                       'result_month': result_month,
+                                                                       'result_avg_money': result_avg_money})
 
     return HttpResponse('fail')
 
