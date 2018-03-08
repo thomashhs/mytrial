6c5f1a0cc8814a9a757d8445a3ced9e0d8bb553d
thomashhs <345870016@qq.com>
2018-03-08 22:39:46 +0800

    5th commit

--- first/forms.py
+++ first/forms.py
@@ -1,18 +1,24 @@
 from django import forms
 
 class CompundForm(forms.Form):
-    base_money = forms.IntegerField(label='本金',initial=100000)
-    base_rate = forms.DecimalField(label='年利率',initial=0.1)
-    base_year = forms.IntegerField(label='年期',initial=60)
-    result_money = forms.CharField(label='本息合计',required=False,initial='不需要填写')
-    result_interest = forms.CharField(label='其中利息',required=False,initial='不需要填写')
+    base_money = forms.IntegerField(label='本金（元）',initial=100000)
+    base_rate = forms.DecimalField(label='年利率（%）',initial=10)
+    base_year = forms.IntegerField(label='年期（年）',initial=60)
+    result_money = forms.CharField(label='本息合计（元）',required=False,initial='不需要填写')
+    result_interest = forms.CharField(label='其中利息（元）',required=False,initial='不需要填写')
 
 class LoanForm(forms.Form):
     loan_type = forms.ChoiceField(label='还款方式',choices = (('等额本息','等额本息'),('等额本金','等额本金'),))
-    loan_money = forms.IntegerField(label='贷款总额',initial=1170000)
-    loan_year = forms.IntegerField(label='贷款年限',initial=30)
-    loan_rate = forms.DecimalField(label='商贷利率',initial=0.0441)
-    result_month = forms.CharField(label='贷款月数', required=False, initial='不需要填写')
-    result_avg_money = forms.CharField(label='月均还款', required=False, initial='不需要填写')
-    result_total_money = forms.CharField(label='还款总额', required=False, initial='不需要填写')
-    result_interest = forms.CharField(label='支付利息', required=False, initial='不需要填写')
+    loan_money = forms.IntegerField(label='贷款总额（元）',initial=1170000)
+    loan_year = forms.IntegerField(label='贷款年限（年）',initial=30)
+    loan_rate = forms.DecimalField(label='商贷利率（%）',initial=4.41)
+    result_month = forms.CharField(label='贷款月数（月）', required=False, initial='不需要填写')
+    result_avg_money = forms.CharField(label='月均还款（元）', required=False, initial='不需要填写')
+    result_total_money = forms.CharField(label='还款总额（元）', required=False, initial='不需要填写')
+    result_interest = forms.CharField(label='支付利息（元）', required=False, initial='不需要填写')
+
+class BmiForm(forms.Form):
+    bmi_height = forms.IntegerField(label='身高（厘米）',initial=176)
+    bmi_weight = forms.DecimalField(label='体重（公斤）',initial=65)
+    bmi_result = forms.CharField(label='BMI指数',required=False,initial='不需要填写')
+    bmi_conclusion = forms.CharField(label='结论',required=False,initial='不需要填写')
--- first/views.py
+++ first/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Title
 from django.http import HttpResponse
 import math
-from .forms import CompundForm,LoanForm
+from .forms import CompundForm,LoanForm,BmiForm
 
 # Create your views here.
 def index(request):
@@ -18,6 +18,9 @@ def detail(request,title_id):
     if title.title_text=='住房贷款计算':
         form=LoanForm()
         return render(request, 'first/detail_1.html', context={'title': title, 'form': form})
+    if title.title_text=='BMI指数':
+        form=BmiForm()
+        return render(request, 'first/detail_1.html', context={'title': title, 'form': form})
     return render(request,'first/detail.html',context={'title':title})
 
 
@@ -32,7 +35,7 @@ def result(request,title_id):
 
             if form.is_valid():
                 base_money = form.cleaned_data['base_money']
-                base_rate = form.cleaned_data['base_rate']
+                base_rate = form.cleaned_data['base_rate']/100
                 base_year = form.cleaned_data['base_year']
 
                 result_money = base_money*pow((1+base_rate),base_year)
@@ -42,14 +45,12 @@ def result(request,title_id):
                 result_interest=round(result_interest,2)
 
                 data={
-                      'loan_type':loan_type,
                       'base_money':base_money,
-                      'base_rate':base_rate,
+                      'base_rate':str(base_rate*100)+'%',
                       'base_year':base_year,
                       'result_money':result_money,
                       'result_interest':result_interest
                      }
-                print(data)
                 form=CompundForm(data)
 
                 return render(request, 'first/result_1.html', context={'title':title,
@@ -62,32 +63,85 @@ def result(request,title_id):
             if form.is_valid():
                 loan_type = form.cleaned_data['loan_type']
                 loan_money = form.cleaned_data['loan_money']
-                loan_rate = form.cleaned_data['loan_rate']/12
+                loan_rate = form.cleaned_data['loan_rate']/100/12
                 loan_year = form.cleaned_data['loan_year']
 
                 result_month = loan_year*12
-                result_avg_money = (loan_money*loan_rate*pow(1+loan_rate,result_month))/ (pow(1+loan_rate,result_month)-1)
-                result_total_money = result_avg_money*result_month
-                result_interest = result_total_money - loan_money
 
-                result_avg_money=round(result_avg_money,2)
-                result_total_money=round(result_total_money,2)
-                result_interest=round(result_interest,2)
+                if loan_type=='等额本息':
+                    result_avg_money = (loan_money*loan_rate*pow(1+loan_rate,result_month))/ (pow(1+loan_rate,result_month)-1)
+                    result_total_money = result_avg_money*result_month
+                    result_interest = result_total_money - loan_money
+                    result_avg_money = round(result_avg_money, 2)
+                    result_total_money = round(result_total_money, 2)
+                    result_interest = round(result_interest, 2)
 
-                data={
-                      'loan_type':loan_type,
-                      'loan_money':loan_money,
-                      'loan_rate':str(round(loan_rate*12*100,3))+'%',
-                      'loan_year':loan_year,
-                      'result_month':result_month,
-                      'result_avg_money':result_avg_money,
-                      'result_total_money':result_total_money,
-                      'result_interest':result_interest
-                     }
+                    data = {
+                        'loan_type': loan_type,
+                        'loan_money': loan_money,
+                        'loan_rate': str(round(loan_rate * 12 * 100, 3)) + '%',
+                        'loan_year': loan_year,
+                        'result_month': result_month,
+                        'result_avg_money': result_avg_money,
+                        'result_total_money': result_total_money,
+                        'result_interest': result_interest
+                    }
+                elif loan_type=='等额本金':
+                    result_avg_money = (loan_money/result_month)+float(loan_money*loan_rate)
+                    result_interest = (1+result_month)*loan_money*loan_rate/2
+                    result_total_money = (1+result_month)*loan_money*loan_rate/2+loan_money
+                    result_descending_money = loan_money/result_month*float(loan_rate)
+
+                    result_avg_money = round(result_avg_money, 2)
+                    result_total_money = round(result_total_money, 2)
+                    result_interest = round(result_interest, 2)
+                    result_descending_money = round(result_descending_money, 2)
+
+                    data = {
+                        'loan_type': loan_type,
+                        'loan_money': loan_money,
+                        'loan_rate': str(round(loan_rate * 12 * 100, 3)) + '%',
+                        'loan_year': loan_year,
+                        'result_month': result_month,
+                        'result_total_money': result_total_money,
+                        'result_avg_money': str(result_avg_money)+'  '+'（首月后每月递减'+str(result_descending_money)+'）',
+                        'result_interest': result_interest
+                    }
 
                 form = LoanForm(data)
                 return render(request, 'first/result_1.html', context={'title': title,
                                                                        'form': form})
+
+    if title.title_text == 'BMI指数':
+        if request.method=='POST':
+            form=BmiForm(request.POST)
+
+            if form.is_valid():
+                bmi_height = form.cleaned_data['bmi_height']/100
+                bmi_weight = form.cleaned_data['bmi_weight']
+                bmi_result = float(bmi_weight)/(bmi_height*bmi_height)
+
+
+                bmi_result=round(bmi_result,2)
+
+                if bmi_result<18.5:
+                    bmi_conclusion = "偏瘦"
+                elif bmi_result>18.5 and bmi_result<=23.9:
+                    bmi_conclusion = "正常"
+                elif bmi_result>=24:
+                    bmi_conclusion = "超重"
+
+                data={
+                      'bmi_height':bmi_height,
+                      'bmi_weight':bmi_weight,
+                      'bmi_result':bmi_result,
+                      'bmi_conclusion':bmi_conclusion,
+                     }
+
+                form=BmiForm(data)
+
+                return render(request, 'first/result_1.html', context={'title':title,
+                                                               'form':form})
 
     return HttpResponse('fail')
 
